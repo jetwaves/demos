ebf3c9c3a30d35ccc0a5fe8f8b0de0c596bc27de
jetwaves@office <jetwaves@office>
2018-09-01 16:15:13 +0800

    simple-git : git clone and git pull tested

--- 3._js_Modules/1_git-js/1. test01_clone.js
+++ 3._js_Modules/1_git-js/1. test01_clone.js
@@ -8,12 +8,35 @@ var Promise = require('promise');
 //var _ = require('underscore');
 //var exec = require('child_process').exec;
 
+var chalk = require('chalk');
 
 try {
-    console.log(require.resolve("mongodb"));
-    console.log(require.resolve("mongojs"));
+    require.resolve("simple-git")
+    // console.log();
 } catch(e) {
-    console.error(chalk.bgRed(' error messages .......'));
+    console.error(chalk.bgRed(' simple-git module is not installed '));
     process.exit(e.code);
 }
+
+const simpleGit = require('simple-git')();
+
+// 1. Clone a repository  ( without login - password )
+//        - Don't need password if git credential is memorised locally
+//        - login:password should be entered in the CLI
+// simpleGit.clone('https://coding.net/u/jetwaves/p/posSite', function(){
+//
+// });
+
+// // 2. Git pull to update
+// simpleGit.pull(function(){
+//
+// });
+
+// // 3. Git pull with branches and  options
+// simpleGit.pull('origin','dev','--force', function(){
+//
+// });
+
+
+
 
